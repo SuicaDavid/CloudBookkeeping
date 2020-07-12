3936dbf55f702d0f4f265167a156f848d961f0f3
David <genglanghuang@gmail.com>
2020-07-12 23:59:21 +0100

    Finished the layout of category list

--- CloudBookkeeping/View/Components/CategoryList.swift
+++ CloudBookkeeping/View/Components/CategoryList.swift
@@ -11,8 +11,8 @@ import SwiftUI
 struct CategoryList<ItemView>: View where ItemView: View {
     var categories: [Category]
     private var viewFormItem: (Category) -> ItemView
-    private let itemWidth: CGFloat = 50
-    private let itemPadding: CGFloat = 10
+    private var itemWidth: CGFloat = 50
+    private var itemPadding: CGFloat = 10
     
     init(categories: [Category], viewFormItem: @escaping (Category) -> ItemView) {
         self.categories = categories
@@ -20,45 +20,53 @@ struct CategoryList<ItemView>: View where ItemView: View {
     }
     
     var body: some View {
-        GeometryReader {
-            geometry in self.bodyView(geometry: geometry)
+        GeometryReader { geometry in
+            self.bodyView(geometry: geometry)
         }
     }
     
     func bodyView(geometry: GeometryProxy) -> some View {
         let count = categories.count
-        let colNumber = self.getColNumber(elementWidth: geometry.size.width)
+        let elementWidth = geometry.size.width
+        let colNumber = self.getColNumber(elementWidth: elementWidth)
         let rowNumber = count / colNumber + 1
+        let calculatedPadding = self.getPadding(elementWidth: elementWidth, colNumber: colNumber)
         return VStack(alignment: .leading, spacing: 0) {
-            Text("\(geometry.size.width)  \(colNumber)")
             ForEach(0..<rowNumber, id: \.self) { row in
                 HStack(alignment: .center, spacing: 0) {
-                    ForEach(0..<self.getColNumber(count: count, colNumber: colNumber, row: row), id: \.self) { col in
+                    ForEach(0..<self.getDynamicColNumber(count: count, colNumber: colNumber, row: row), id: \.self) { col in
                         Group {
                             self.viewFormItem(self.categories[self.getIndexFromList(row: row, col: col, colNumber: colNumber)])
                         }
-                        .padding(.bottom, self.itemPadding)
-                        .padding(.trailing, self.getPadding(count: count, colNumber: colNumber, row: row, col: col))
+                        .padding(.bottom, calculatedPadding)
+                        .padding(.trailing, self.getLastPadding(padding: calculatedPadding, count: count, colNumber: colNumber, row: row, col: col))
                     }
                 }
             }
         }
     }
-    func getColNumber(count: Int, colNumber: Int, row: Int) -> Int {
-        return min(colNumber, (count - colNumber * row))
+    func getPadding(elementWidth: CGFloat, colNumber: Int) -> CGFloat {
+        let gap = elementWidth - ((self.itemPadding + self.itemWidth) * CGFloat(colNumber) - self.itemPadding)
+        return gap == 0 ? self.itemPadding : self.itemPadding + gap / CGFloat(colNumber)
     }
     
-    func getPadding(count: Int, colNumber: Int, row: Int, col: Int) -> CGFloat {
-        return col != getColNumber(count: count, colNumber: colNumber, row: row) - 1 ? self.itemPadding : 0
+    func getLastPadding(padding: CGFloat, count: Int, colNumber: Int, row: Int, col: Int) -> CGFloat {
+        return col != getDynamicColNumber(count: count, colNumber: colNumber, row: row) - 1 ? padding : 0
     }
     
     func getIndexFromList(row: Int, col: Int, colNumber: Int) -> Int {
         return col + row * colNumber
     }
-
+    
     func getColNumber(elementWidth: CGFloat) -> Int {
-        return (Int(elementWidth) / Int(itemWidth))
+        let originalColNumber = Int(elementWidth) / Int(itemWidth + itemPadding)
+        let gap = elementWidth - ((self.itemPadding + self.itemWidth) * CGFloat(originalColNumber) - self.itemPadding)
+        return gap > self.itemWidth ? originalColNumber + 1 : originalColNumber
     }
+    
+    func getDynamicColNumber(count: Int, colNumber: Int, row: Int) -> Int {
+           return min(colNumber, (count - colNumber * row))
+       }
 }
 
 struct CategoryList_Previews: PreviewProvider {
